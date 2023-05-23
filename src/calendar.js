8730c4c91158cf1e20a0e9fa9ecace2cf6d5fe41
leguchi <35780958+leguchi@users.noreply.github.com>
2023-05-22 21:08:37 -0400

    Added calendar

---
+++ src/calendar.js
@@ -0,0 +1,268 @@
+function nextCalendar() {
+    // let current = new Date();
+    // let year = current.getFullYear();
+    // let month = current.getMonth();
+    // let date = current.getDate();
+
+    let monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+
+    // default setup
+    let defaultDisplay = document.querySelector(".calendar h2");
+    // if (defaultDisplay.textContent != monthName[month]) {
+    //     defaultDisplay.textContent = monthName[month];
+    // }
+    console.log(defaultDisplay);
+    let i = monthName.findIndex((mo) => mo == defaultDisplay.textContent);
+    console.log('i', i);
+    if (i == 11) {
+        i = 0;
+    } else {
+        i += 1;
+    }
+    defaultDisplay.textContent = monthName[i];
+
+}
+
+function backCalendar() {
+    // let current = new Date();
+    // let year = current.getFullYear();
+    // let month = current.getMonth();
+    // let date = current.getDate();
+
+    let monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+
+    // default setup
+    let defaultDisplay = document.querySelector(".calendar h2");
+    // if (defaultDisplay.textContent != monthName[month]) {
+    //     defaultDisplay.textContent = monthName[month];
+    // }
+    console.log(defaultDisplay);
+    let i = monthName.findIndex((mo) => mo == defaultDisplay.textContent);
+    console.log('i', i);
+    if (i == 0) {
+        i = 11;
+    } else {
+        i -= 1;
+    }
+    defaultDisplay.textContent = monthName[i];
+
+}
+
+function dayIncluded() {
+    let defaultDisplay = document.querySelector(".calendar h2");
+    let current = new Date();
+    let year = current.getFullYear();
+    let month = current.getMonth();
+    let date = current.getDate();
+
+    let monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    if (month < 9) {
+        month = '0' + (month + 1);
+    }
+    else {
+        month = (month + 1).toString();
+    }
+    let monthStart = new Date(`${year}-${month}-01`);
+    console.log(monthStart);
+    let weekday = {
+        0: 'Su',
+        1: 'Mo',
+        2: 'Tu',
+        3: 'We',
+        4: 'Th',
+        5: 'Fr',
+        6: 'Sa'
+    };
+    let dow = weekday[monthStart.getDay()];
+    let numOfDaysFeb;
+    if (year % 4 == 0) {
+        numOfDaysFeb = 29;
+    }
+    else {
+        numOfDaysFeb = 28;
+    }
+    let numOfDays = {
+        'January': 31,
+        'February': numOfDaysFeb,
+        'March': 31,
+        'April': 30,
+        'May': 31,
+        'June': 30,
+        'July': 31,
+        'August': 31,
+        'September': 30,
+        'October': 31,
+        'November': 30,
+        'December': 31
+    };
+    // let firstWeek = document.querySelector(".calendar table tbody tr td");
+    switch (dow) {
+        case 'Mo':
+            console.log('mo');
+            for (let i = 0; i < numOfDays[i]; i++) {
+                document.querySelectorAll(".calendar table tbody tr td")[i].style.color = "#FFFFFF";
+                document.querySelectorAll(".calendar table tbody tr td")[i].textContent = `${i + 1}`;
+            }
+            break;
+        case 'Tu':
+            {
+                console.log('tu');
+                let i = monthName.findIndex((mo) => mo == defaultDisplay.textContent);
+                // console.log('i', i);
+                if (i == 0) {
+                    i = 11;
+                } else {
+                    i -= 1;
+                }
+                for (let index = 0; index < numOfDays[i]; index++) {
+                    if (index == 0) {
+                        document.querySelectorAll(".calendar table tbody tr td")[index].style.color = "#A1A5A1";
+                        document.querySelectorAll(".calendar table tbody tr td")[index].textContent = `${numOfDays[i]}`;
+                    }
+                    else {
+                        document.querySelectorAll(".calendar table tbody tr td")[index].style.color = "#FFFFFF";
+                        document.querySelectorAll(".calendar table tbody tr td")[index].textContent = `${index + 1}`;
+                    }
+                }
+            }
+            break;
+        case 'We':
+            {
+                console.log('we');
+                let i = monthName.findIndex((mo) => mo == defaultDisplay.textContent);
+                // console.log('i', i);
+                if (i == 0) {
+                    i = 11;
+                } else {
+                    i -= 1;
+                }
+                for (let index = 0; index < numOfDays[i]; index++) {
+                    if (index <= 1) {
+                        document.querySelectorAll(".calendar table tbody tr td")[index].style.color = "#A1A5A1";
+                        document.querySelectorAll(".calendar table tbody tr td")[index].textContent = `${numOfDays[i]}`;
+                    }
+                    else {
+                        document.querySelectorAll(".calendar table tbody tr td")[index].style.color = "#FFFFFF";
+                        document.querySelectorAll(".calendar table tbody tr td")[index].textContent = `${index + 1}`;
+                    }
+                }
+            }
+            break;
+        case 'Th':
+            {
+                console.log('th');
+                let i = monthName.findIndex((mo) => mo == defaultDisplay.textContent);
+                // console.log('i', i);
+                if (i == 0) {
+                    i = 11;
+                } else {
+                    i -= 1;
+                }
+                for (let index = 0; index < numOfDays[i]; index++) {
+                    if (index <= 2) {
+                        document.querySelectorAll(".calendar table tbody tr td")[index].style.color = "#A1A5A1";
+                        document.querySelectorAll(".calendar table tbody tr td")[index].textContent = `${numOfDays[i]}`;
+                    }
+                    else {
+                        document.querySelectorAll(".calendar table tbody tr td")[index].style.color = "#FFFFFF";
+                        document.querySelectorAll(".calendar table tbody tr td")[index].textContent = `${index + 1}`;
+                    }
+                }
+            }
+            break;
+        case 'Fr':
+            {
+                console.log('fr');
+                let i = monthName.findIndex((mo) => mo == defaultDisplay.textContent);
+                // console.log('i', i);
+                if (i == 0) {
+                    i = 11;
+                } else {
+                    i -= 1;
+                }
+                for (let index = 0; index < numOfDays[i]; index++) {
+                    if (index <= 3) {
+                        document.querySelectorAll(".calendar table tbody tr td")[index].style.color = "#A1A5A1";
+                        document.querySelectorAll(".calendar table tbody tr td")[index].textContent = `${numOfDays[i]}`;
+                    }
+                    else {
+                        document.querySelectorAll(".calendar table tbody tr td")[index].style.color = "#FFFFFF";
+                        document.querySelectorAll(".calendar table tbody tr td")[index].textContent = `${index + 1}`;
+                    }
+                }
+            }
+            break;
+        case 'Sa':
+            {
+                console.log('sa');
+                let i = monthName.findIndex((mo) => mo == defaultDisplay.textContent);
+                // console.log('i', i);
+                if (i == 0) {
+                    i = 11;
+                } else {
+                    i -= 1;
+                }
+                for (let index = 0; index < numOfDays[i]; index++) {
+                    if (index <= 4) {
+                        document.querySelectorAll(".calendar table tbody tr td")[index].style.color = "#A1A5A1";
+                        document.querySelectorAll(".calendar table tbody tr td")[index].textContent = `${numOfDays[i]}`;
+                    }
+                    else {
+                        document.querySelectorAll(".calendar table tbody tr td")[index].style.color = "#FFFFFF";
+                        document.querySelectorAll(".calendar table tbody tr td")[index].textContent = `${index + 1}`;
+                    }
+                }
+            }
+            break;
+        case 'Su':
+            {
+                console.log('su');
+                let i = monthName.findIndex((mo) => mo == defaultDisplay.textContent);
+                // console.log('i', i);
+                if (i == 11) {
+                    i = 0;
+                } else {
+                    i += 1;
+                }
+                for (let index = 0; index < numOfDays[i]; index++) {
+                    // console.log(document.querySelectorAll(".calendar table tbody tr td"));
+                    if (index <= 5) {
+                        document.querySelectorAll(".calendar table tbody tr td")[index].style.color = "#A1A5A1";
+                        document.querySelectorAll(".calendar table tbody tr td")[index].textContent = `${numOfDays[i]}`;
+                    }
+                    else {
+                        document.querySelectorAll(".calendar table tbody tr td")[index].style.color = "#FFFFFF";
+                        document.querySelectorAll(".calendar table tbody tr td")[index].textContent = `${index + 1}`;
+                    }
+                }
+            }
+            break;
+        default:
+            break;
+    }
+
+}
+
+function changeNextMonth() {
+    nextCalendar();
+    dayIncluded();
+}
+
+function changePrevMonth() {
+    backCalendar();
+    dayIncluded();
+}
+
+function curMonth() {
+    let monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let today = new Date();
+    let month = today.getMonth();
+    let listedMonth = document.querySelector(".calendar h2");
+    listedMonth.textContent = monthName[month];
+}
+
+
+export { changeNextMonth, changePrevMonth, curMonth };
--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -2,19 +2,81 @@
 
 import { Link } from 'react-router-dom';
 import './../../style.css';
+import './../../menu.css';
+import { changeNextMonth, changePrevMonth, curMonth } from '../../calendar';
+// import './../../index';
 
 
 const HomePageView = () => {
   return (
-    <div class="grid_container">
+    <div className="grid_container">
       <h1 id="header">CALENDAR</h1>
-      <div class="calendar">
+      <form id="app-cover">
+  <div id="select-box">
+    <input type="checkbox" id="options-view-button"></input>
+    <div id="select-button" className="brd">
+      <div id="selected-value">
+        <span>Select a platform</span>
+      </div>
+      <div id="chevrons">
+        <i className="fas fa-chevron-up"></i>
+        <i className="fas fa-chevron-down"></i>
+      </div>
+    </div>
+    <div id="options">
+      <div className="option">
+        <input className="s-c top" type="radio" name="platform" value="codepen"></input>
+        <input className="s-c bottom" type="radio" name="platform" value="codepen"></input>
+        <i className="fab fa-codepen"></i>
+        <span className="label">CodePen</span>
+        <span className="opt-val">CodePen</span>
+      </div>
+      <div className="option">
+        <input className="s-c top" type="radio" name="platform" value="dribbble"></input>
+        <input className="s-c bottom" type="radio" name="platform" value="dribbble"></input>
+        <i className="fab fa-dribbble"></i>
+        <span className="label">Dribbble</span>
+        <span className="opt-val">Dribbble</span>
+      </div>
+      <div className="option">
+        <input className="s-c top" type="radio" name="platform" value="behance"></input>
+        <input className="s-c bottom" type="radio" name="platform" value="behance"></input>
+        <i className="fab fa-behance"></i>
+        <span className="label">Behance</span>
+        <span className="opt-val">Behance</span>
+      </div>
+      <div className="option">
+        <input className="s-c top" type="radio" name="platform" value="hackerrank"></input>
+        <input className="s-c bottom" type="radio" name="platform" value="hackerrank"></input>
+        <i className="fab fa-hackerrank"></i>
+        <span className="label">HackerRank</span>
+        <span className="opt-val">HackerRank</span>
+      </div>
+      <div className="option">
+        <input className="s-c top" type="radio" name="platform" value="stackoverflow"></input>
+        <input className="s-c bottom" type="radio" name="platform" value="stackoverflow"></input>
+        <i className="fab fa-stack-overflow"></i>
+        <span className="label">StackOverflow</span>
+        <span className="opt-val">StackOverflow</span>
+      </div>
+      <div className="option">
+        <input className="s-c top" type="radio" name="platform" value="freecodecamp"></input>
+        <input className="s-c bottom" type="radio" name="platform" value="freecodecamp"></input>
+        <i className="fab fa-free-code-camp"></i>
+        <span className="label">FreeCodeCamp</span>
+        <span className="opt-val">FreeCodeCamp</span>
+      </div>
+      <div id="option-bg"></div>
+    </div>
+  </div>
+</form>
+      <div className="calendar">
         <header>
 
-          <h2>September</h2>
+          <h2 onLoad={curMonth}></h2>
 
-          <a class="btn-prev fontawesome-angle-left" href="#"></a>
-          <a class="btn-next fontawesome-angle-right" href="#"></a>
+          <button className="btn-prev fontawesome-angle-left" onClick={changePrevMonth}></button>
+          <button className="btn-next fontawesome-angle-right" onClick={changeNextMonth}></button>
 
         </header>
 
@@ -22,7 +84,7 @@ const HomePageView = () => {
 
           <thead>
 
-            <tr class="dow">
+            <tr className="dow">
 
               <td>Mo</td>
               <td>Tu</td>
@@ -39,68 +101,67 @@ const HomePageView = () => {
           <tbody>
 
             <tr>
-              <td class="prev-month">26</td>
-              <td class="prev-month">27</td>
-              <td class="prev-month">28</td>
-              <td class="prev-month">29</td>
-              <td class="prev-month">30</td>
-              <td class="prev-month">31</td>
-              <td>1</td>
+              <td className="prev-month">26</td>
+              <td className="prev-month">27</td>
+              <td className="prev-month">28</td>
+              <td className="prev-month">29</td>
+              <td className="prev-month">30</td>
+              <td className="prev-month">31</td>
+              <td className="curMonth">1</td>
             </tr>
             <tr>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-              <td>7</td>
-              <td>8</td>
+              <td className="curMonth">2</td>
+              <td className="curMonth">3</td>
+              <td className="curMonth">4</td>
+              <td className="curMonth">5</td>
+              <td className="curMonth">6</td>
+              <td className="curMonth">7</td>
+              <td className="curMonth">8</td>
             </tr>
             <tr>
-              <td>9</td>
-              <td class="event">10</td>
-              <td>11</td>
-              <td>12</td>
-              <td>13</td>
-              <td>14</td>
-              <td>15</td>
+              <td className="curMonth">9</td>
+              <td className="event curMonth">10</td>
+              <td className="curMonth">11</td>
+              <td className="curMonth">12</td>
+              <td className="curMonth">13</td>
+              <td className="curMonth">14</td>
+              <td className="curMonth">15</td>
             </tr>
             <tr>
-              <td>16</td>
-              <td>17</td>
-              <td>18</td>
-              <td>19</td>
-              <td>20</td>
-              <td class="event">21</td>
-              <td>22</td>
+              <td className="curMonth">16</td>
+              <td className="curMonth">17</td>
+              <td className="curMonth">18</td>
+              <td className="curMonth">19</td>
+              <td className="curMonth">20</td>
+              <td className="event curMonth">21</td>
+              <td className="curMonth">22</td>
             </tr>
 
             <tr>
-              <td class="current-day event">23</td>
-              <td>24</td>
-              <td>25</td>
-              <td>26</td>
-              <td>27</td>
-              <td>28</td>
-              <td>29</td>
+              <td className="current-day event curMonth">23</td>
+              <td className="curMonth">24</td>
+              <td className="curMonth">25</td>
+              <td className="curMonth">26</td>
+              <td className="curMonth">27</td>
+              <td className="curMonth">28</td>
+              <td className="curMonth">29</td>
             </tr>
             <tr>
-              <td>30</td>
-              <td class="next-month">1</td>
-              <td class="next-month">2</td>
-              <td class="next-month">3</td>
-              <td class="next-month">4</td>
-              <td class="next-month">5</td>
-              <td class="next-month">6</td>
+              <td className="curMonth">30</td>
+              <td className="next-month">1</td>
+              <td className="next-month">2</td>
+              <td className="next-month">3</td>
+              <td className="next-month">4</td>
+              <td className="next-month">5</td>
+              <td className="next-month">6</td>
             </tr>
 
           </tbody>
 
         </table>
       </div>
-      <Link to={'/instructors'} > All Instructors </Link>
-      <Link to={'/courses'} > All Courses </Link>
-
+      {/* <Link to={'/instructors'} > All Instructors </Link>
+      <Link to={'/courses'} > All Courses </Link> */}
     </div>
   );
 }
